3bee7620c8b63e110bdd531e6aad50a5ed7365f7
michaelkurniawan <mchlkk98@gmail.com>
2025-07-30 13:17:15 +0700

    MIKEL | try fix on production

--- Kainara/resources/views/layouts/app.blade.php
+++ Kainara/resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" xintegrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-    @include('notification.notif-components')
+    @include('components.notif-components')
 
     @stack('scripts')
 </body>
