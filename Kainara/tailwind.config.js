f2e6d188d397b54f44997f191b9d89ac16af340f
michaelkurniawan <mchlkk98@gmail.com>
2025-07-30 13:27:03 +0700

    MIKEL | Fix tailwind

--- Kainara/tailwind.config.js
+++ Kainara/tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
@@ -5,9 +6,19 @@ import forms from '@tailwindcss/forms';
 export default {
     darkMode: 'class',
     content: [
+        // Ini OK, untuk memastikan styling Pagination Laravel bekerja
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        
+        // HAPUS BARIS INI: Tailwind TIDAK PERLU membaca file cache PHP
+        // './storage/framework/views/*.php', 
+        
+        // INI PENTING DAN HARUS ADA: Semua Blade view kustom Anda
+        './resources/views/**/*.blade.php', 
+
+        // Tambahkan jika Anda menggunakan JS/Vue/React/Livewire di resources/js/
+        './resources/js/**/*.js',
+        './resources/js/**/*.vue',
+        './resources/js/**/*.ts', // Jika menggunakan TypeScript
     ],
 
     theme: {
